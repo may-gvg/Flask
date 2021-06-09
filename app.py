9401677eb3068b95be97fe3bd5f6e072344e26b2
may <83417030+may-gvg@users.noreply.github.com>
2021-06-09 17:56:13 +0200

    update app.py /contact/me bez post

--- app.py
+++ app.py
@@ -21,16 +21,12 @@ def tata():
 
 
 
-@app.route ('/mypage/me', methods=['GET', 'POST'])
+@app.route ('/mypage/me', methods=['GET'])
 def mypage():
    if request.method == 'GET':
        print("We received GET")
        return render_template("me.html")
-   elif request.method == 'POST':
-       print("We received POST")
-       print(request.form)
-       return redirect("/")
-
+   
 
 @app.route ('/mypage/contact', methods=['GET', 'POST'])
 def contact():
